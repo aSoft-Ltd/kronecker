15022d51cf1933b839087c069a02a3b0f2465dd3
andylamax <andylamax@programmer.net>
2023-04-27 02:17:53 +0300

    Added kronecker to oss

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,9 +7,9 @@ plugins {
 kotlin {
     jvm { library() }
     if (Targeting.JS) js(IR) { library() }
-    if (Targeting.WASM) wasm { library() }
+//    if (Targeting.WASM) wasm { library() }
     val osxTargets = if (Targeting.OSX) osxTargets() else listOf()
-    val ndkTargets = if (Targeting.NDK) ndkTargets() else listOf()
+//    val ndkTargets = if (Targeting.NDK) ndkTargets() else listOf()
     val linuxTargets = if (Targeting.LINUX) linuxTargets() else listOf()
     val mingwTargets = if (Targeting.MINGW) mingwTargets() else listOf()
 
