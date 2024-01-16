03291ffa2b2622466a25048881286bc8cdd7abdc
andylamax <andylamax@programmer.net>
2024-01-16 18:45:32 +0300

    Restrucing for better testing

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,7 +9,8 @@ description = "A suit of libraries for data pagination and paging at large"
 kotlin {
     jvm { library() }
     if (Targeting.JS) js(IR) { library() }
-//    if (Targeting.WASM) wasm { library() }
+    if (Targeting.WASM) wasmJs { library() }
+    if (Targeting.WASM) wasmWasi { library() }
     val osxTargets = if (Targeting.OSX) osxTargets() else listOf()
 //    val ndkTargets = if (Targeting.NDK) ndkTargets() else listOf()
     val linuxTargets = if (Targeting.LINUX) linuxTargets() else listOf()
